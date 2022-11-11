33170acfe82fe0b3d9bd2569e43adb833eeadd12
Flore <debosscher.flore@gmail.com>
2022-11-11 14:46:48 +0100

    Prototype

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 window.onload = function (){
-    fetch("posts.xml").then(content => {
+    fetch("posts.xml").then(content => content.text()).then(text => {
         let parser = new DOMParser();
-        let xmlDoc = parser.parseFromString(parser, "text/xml");
-        console.log(xmlDoc)
+        let xmlDoc = parser.parseFromString(text, "text/xml");
+        let posts = xmlDoc.getElementsByTagName("post")
+        for(let post of posts){
+            let container = document.createElement("div")
+            container.innerHTML = "<h1>" + get(post, "title") + "</h1>"
+            let content = document.createElement("img")
+            content.src = get(post, "photo")
+            container.appendChild(content)
+            let desc = "<p>" + get(post, "description") + "</p>"
+            container.innerHTML += desc
+            document.body.appendChild(container)
+        }
     })
+}
+
+
+function get(post, key){
+    console.log(post)
+    return post.getElementsByTagName(key)[0].innerHTML
 }
